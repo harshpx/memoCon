162342af40ada4553965fc8758751189e1701664
harshpx <harsh.rzf@gmail.com>
2024-02-08 00:25:42 +0530

    resolving deployment issues

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import http from 'http';
 import colors from 'colors';
 import connectDB from './utils/db.js';
 import errorHandler from './middlewares/errorMiddleware.js';
@@ -32,11 +33,11 @@ app.use('/api/users',userRoutes);
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
-
+const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log(`Database connected`);
-    app.listen(port,()=>{
+    server.listen(port,()=>{
         console.log(`Server running on port: ${port}`);
     })
 }).catch(err=>{
