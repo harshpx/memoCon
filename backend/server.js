b49a413bc3f3ffde67c12b5e6564a91f341f72fd
harshpx <harsh.rzf@gmail.com>
2024-02-07 23:22:25 +0530

    setting up for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,19 @@ import connectDB from './utils/db.js';
 import errorHandler from './middlewares/errorMiddleware.js';
 import noteRoutes from './routes/noteRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 connectDB();
+
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
