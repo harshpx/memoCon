da76b1dd1d8b22ed6f8805a5a37d6bbce0dd6d9b
harshpx <harsh.rzf@gmail.com>
2024-02-08 07:00:07 +0530

    fixing dp upload function

--- frontend/src/components/ImageCropper.jsx
+++ frontend/src/components/ImageCropper.jsx
@@ -111,7 +111,7 @@ const dataURLtoFile = (dataurl, filename) => {
         formData.append('upload_preset',`${import.meta.env.VITE_CLOUD_UPLOAD_PRESET}`);
         formData.append('cloud_name',`${import.meta.env.VITE_CLOUD_NAME}`);
         console.log(String(import.meta.env.VITE_CLOUD_UPLOAD_PRESET), String(import.meta.env.VITE_CLOUD_NAME));
-        fetch(`https://api.cloudinary.com/v1_1/${String(import.meta.env.VITE_CLOUD_NAME)}/image/upload`,{method:"post",body:formData})
+        axios.post(`https://api.cloudinary.com/v1_1/${String(import.meta.env.VITE_CLOUD_NAME)}/image/upload`,formData)
         .then(response=>response.json())
         .then(response=>dispatch(userProfilePicture(response.url)))
         .catch(error=>console.log(error))
